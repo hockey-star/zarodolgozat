e747b7b155469063248638e07a2748acb37e2bd0
hockey-star <pasztor.alex.1216@dszcbaross.edu.hu>
2025-12-16 12:45:46 +0100

    Trailer gomb

--- frontend/src/components/Trailer.jsx
+++ frontend/src/components/Trailer.jsx
@@ -1,5 +1,6 @@
 // frontend/src/components/Trailer.jsx
 import React, { useEffect, useRef } from "react";
+import "./Trailer.css";
 
 export default function Trailer({ onEnd }) {
   const ref = useRef();
@@ -17,7 +18,7 @@ export default function Trailer({ onEnd }) {
   return (
     <div className="fixed inset-0 bg-black flex items-center justify-center">
       <video ref={ref} src="/src/assets/videos/trailer.mp4" className="w-full h-full object-cover" />
-      <button onClick={() => onEnd()} className="absolute bottom-10 right-10 bg-yellow-500 text-black px-6 py-3 rounded-xl font-bold hover:bg-yellow-600">Skip</button>
+      <button onClick={() => onEnd()} className="skip absolute bottom-10 right-10  px-6 py-3 ">Skip</button>
     </div>
   );
 }
